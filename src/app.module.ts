c127a3dedfd33eab7bcd6936048548565fe4404c
Vinzh <kevin.zhong@gmail.com>
2025-07-24 17:02:56 +0700

    employee crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmployeeController } from './employee.controller';
+import { EmployeeService } from './employee.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, EmployeeController],
+  providers: [AppService, EmployeeService],
 })
 export class AppModule {}
