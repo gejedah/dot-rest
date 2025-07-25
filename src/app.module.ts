fdef52b2f08ea4c42b3709df8cfaa3492697778c
Vinzh <kevin.zhong@gmail.com>
2025-07-25 10:32:49 +0700

    module typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,32 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeeController } from './employee.controller';
 import { EmployeeService } from './employee.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes ConfigModule available globally
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres', // or 'mysql', 'sqlite', etc.
+        host: configService.get<string>('DB_HOST'),
+        port: parseInt(configService.get<string>('DB_PORT')),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'], // Path to your entities
+        synchronize: false, // **IMPORTANT: Set to false in production! Use migrations.**
+        logging: true, // Enable TypeORM logging (useful for development)
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule
+  ],
   controllers: [AppController, EmployeeController],
   providers: [AppService, EmployeeService],
 })
