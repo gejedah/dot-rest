212845ee195580bad825d70fd9a1d4907a92950e
Vinzh <kevin.zhong@gmail.com>
2025-07-26 11:18:41 +0700

    dml module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { User } from './user.entity';
+import { Transaction } from 'src/transaction/transaction.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])], // Register User entity for this module
+  imports: [TypeOrmModule.forFeature([User, Transaction])], // Register User entity for this module
   providers: [UserService],
   controllers: [UserController],
   exports: [TypeOrmModule], // If other modules need to use User repository
